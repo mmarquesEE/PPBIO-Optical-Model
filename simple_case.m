aea716b3b4c94c0edf6a0d495eb4767869ebd548
mmarquesEE <133893009+mmarquesEE@users.noreply.github.com>
2025-03-24 14:42:25 -0300

    working on the text

--- simple_case.m
+++ simple_case.m
@@ -1,8 +1,21 @@
+% Inclua o pacote de simbolos
+syms d1 d2 d3 d4 f_c f_f n_p
 
+% Defina as matrizes simbólicas
+MLc1 = [1, d1; -1/f_c, 1 - d1/f_c];
+MLf = [1, d2; -1/f_f, 1 - d2/f_f];
+MS1 = [1, d3; 0, 1/n_p];
+Msa = [1, d4; 0, 1];
 
-k_vec = @(z,p)[
-    (- k*p^2*z^2 + k*p^2*z0^2 + 2*k*z^4 ...
-    + 4*k*z^2*z0^2 - 2*z^2*z0 + 2*k*z0^4 - 2*z0^3)/(2*(z^2 + z0^2)^2);
-    (k*p*z)/(z^2 + z0^2)
-];
+% Calcule o produto das matrizes
+M1 = Msa * MS1 * MLf * MLc1;
 
+% Simplifique o resultado, se necessário
+M1 = simplify(M1);
+
+A = M1(1,1); B = M1(1,2); C = M1(2,1); D = M1(2,2);
+
+latex(A)
+latex(B)
+latex(C)
+latex(D)
