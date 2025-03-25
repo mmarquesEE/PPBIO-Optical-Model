38a84373105deb09bd209ff3f44aa72f3879d0e0
mmarquesEE <133893009+mmarquesEE@users.noreply.github.com>
2025-03-25 18:43:28 -0300

    finishing beam parameter calculation

--- simple_case.m
+++ simple_case.m
@@ -2,8 +2,12 @@ clear all
 clc
 
 % Inclua o pacote de simbolos
-syms lambda theta0 z
+syms lambda theta0 z f_c f_f n_p
 
 W0 = lambda/(pi*theta0);
 z0 = lambda/(pi*theta0^2);
+
+z0c = (f_c^2/(2*z0^2))*z0;
+z0f = z0c/(1 + (z0c/f_f)^2);
+z0r = n_p*z0f;
 
